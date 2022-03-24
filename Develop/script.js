dcdd4033ecaf3a943c2400434cb7ee77a1d2bbff
ryann127 <ryann.goldberg127@gmail.com>
2022-03-24 16:14:59 -0600

    added code to change colors of schedule before current time

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,31 @@
 var timeDisplayEl = $("#currentDay");
-var projectDisplayEl = $("#project-display");
-var projectModalEl = $("#project-modal");
-var projectFormEl = $("#project-form");
-var projectNameInputEl = $("#project-name-input");
-var projectTypeInputEl = $("#project-type-input");
-var hourlyRateInputEl = $("#hourly-rate-input");
-var dueDateInputEl = $("#due-date-input");
-
+var presentTime; 
+var hoursInDay = {
+    morning: ["12AM", "01AM", "02AM", "03AM", "04AM", "05AM", "06AM", "07AM", "08AM"],
+    business: ["09AM", "10AM", "11AM", "12PM", "01PM", "02PM", "03PM", "O4PM", "05PM"],
+    night: ["06PM", "07PM", "08PM", "09PM", "10PM", "11PM"]
+};
 
 // TODO: function to display the time in the jumbotron and update every second
 function displayTime() {
-    var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
+    var rightNow = moment().format('MMMM Do YYYY, h:mm:ss A');
     timeDisplayEl.text(rightNow);
 }
 
 setInterval(displayTime, 1000);
+
+function colorChange() {
+    presentTime = moment().format("hhA");
+
+    if (hoursInDay.morning.indexOf(presentTime) !== -1 || hoursInDay.night.indexOf(presentTime) !== -1){
+        $(".hourNotes").css("background-color", "#d3d3d3");
+
+    }
+    if (hoursInDay.business.indexOf(presentTime) !== -1){
+        $("#" + presentTime).css("background-color", "#ff69b4");
+    }
+    for (var i=0; i < hoursInDay.business.indexOf(presentTime); i++){
+        $("#"+presentTime).css("background-color", "#d3d3d3");
+    }
+    //figure out a way to change color for future hours
+}
